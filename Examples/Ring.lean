680fb68459cefdf5248057ef235576d632b6b7a7
George Pîrlea <george@pirlea.net>
2024-02-13 21:05:01 +0800

    WIP: Ring example

--- Examples/Ring.lean
+++ Examples/Ring.lean
@@ -2,6 +2,8 @@ import LeanSts.State
 import LeanSts.TransitionSystem
 import LeanSts.Testing
 
+import Auto
+
 -- https://github.com/aman-goel/ivybench/blob/5db7eccb5c3bc2dd14dfb58eddb859b036d699f5/ex/ivy/ring.ivy
 
 section Ring
@@ -15,7 +17,7 @@ structure TotalOrder (t : Type) :=
   le_antisymm (x y : t) : le x y → le y x → x = y
   le_total    (x y : t) : le x y ∨ le y x
 
-structure Between (t : Type) :=
+structure Between (node : Type) :=
   -- relation: btw represents a ring
   btw (w x y : node) : Bool
   -- axioms
@@ -24,12 +26,148 @@ structure Between (t : Type) :=
   btw_side    (w x y : node) : btw w x y → ¬ btw w y x
   btw_total   (w x y : node) : btw w x y ∨ btw w y x ∨ w = x ∨ w = y ∨ x = y
 
-structure RingStructure (node : Type) :=
+structure Structure (node : Type) [DecidableEq node]  :=
   -- immutable relations & axioms
   total_order : TotalOrder node
   between : Between node
   -- actual state
   leader (n : node) : Bool
   pending (n1 n2 : node) : Bool
+
+def initialState? (node : Type) [DecidableEq node] (rs : Structure node) : Prop :=
+  (∀ (n : node), ¬ rs.leader n) ∧
+  (∀ (n1 n2 : node), ¬ rs.pending n1 n2)
+
+def send (node : Type) [DecidableEq node] : RelationalTransitionSystem.action (Structure node) :=
+  λ (st st' : Structure node) =>
+    ∃ (n next : node),
+      -- preconditions
+      (∀ (z : node), n ≠ next ∧ ((z ≠ n ∧ z ≠ next) → st.between.btw n next z)) ∧
+      -- postconditions
+      st' = {st with pending := st.pending[n , next ↦ true]}
+
+def recv (node : Type) [DecidableEq node] : RelationalTransitionSystem.action (Structure node) :=
+  λ (st st' : Structure node) =>
+    ∃ (sender n next : node) (havoc : Bool),
+      -- preconditions
+      (∀ (z : node), n ≠ next ∧ ((z ≠ n ∧ z ≠ next) → st.between.btw n next z)) ∧
+      (st.pending sender n) ∧
+      -- postconditions
+      -- `pending(sender, n) := *` is modelled using `havoc`
+      if sender = n then
+        st' = {st with leader := st.leader[n ↦ true], pending := st.pending[sender, n ↦ havoc]}
+      else
+        if st.total_order.le n sender then
+          st' = {st with pending := st.pending[sender, n ↦ havoc][n , next ↦ false]}
+        else
+          st' = {st with pending := st.pending[sender, n ↦ havoc]}
+
+instance System {node : Type} [DecidableEq node] :
+  RelationalTransitionSystem (Structure node)
+  where
+  init := λ st => initialState? node st
+  -- TLA-style
+  next := λ st st' => send node st st' ∨ recv node st st'
+
+def safety {node : Type} [DecidableEq node] (st : Structure node) : Prop :=
+  ∀ (N L : node), st.leader L → st.total_order.le N L
+
+def inv_1 {node : Type} [DecidableEq node] (st : Structure node) : Prop :=
+  ∀ (S D N : node), st.pending S D ∧ st.between.btw S N D → st.total_order.le N S
+
+def inv_2 {node : Type} [DecidableEq node] (st : Structure node) : Prop :=
+  ∀ (N L : node), st.pending L L → st.total_order.le N L
+
+-- set_option pp.notation false
+
+def safety_init {node : Type} [DecidableEq node] :
+  ∀ (st : Structure node), System.init st → safety st := by
+  intro st
+  simp only [RelationalTransitionSystem.init, safety, System, initialState?]
+  rintro ⟨hleader, _hpending⟩
+  intro N L hcontra
+  specialize hleader L
+  contradiction
+
+def inv {node : Type} [DecidableEq node] (st : Structure node) : Prop :=
+  safety st ∧ inv_1 st ∧ inv_2 st
+
+def inv_init {node : Type} [DecidableEq node] :
+  ∀ (st : Structure node), System.init st → inv st := by
+  intro st
+  simp only [RelationalTransitionSystem.init, safety, System, initialState?]
+  rintro ⟨hleader, hpending⟩
+  apply And.intro
+  {
+    apply safety_init
+    simp only [RelationalTransitionSystem.init, initialState?]
+    constructor
+    { exact hleader }
+    { exact hpending }
+  }
+  apply And.intro
+  {
+    rintro S D N ⟨hcontra, _hbtw⟩
+    specialize hpending S D
+    contradiction
+  }
+  {
+    intro N L hcontra
+    specialize hpending L L
+    contradiction
+  }
+
+set_option auto.smt true
+set_option trace.auto.printLemmas true
+set_option trace.auto.smt.printCommands true
+set_option trace.auto.smt.result true
+
+theorem inv_inductive {node : Type} [DecidableEq node] :
+  ∀ (st st' : Structure node), System.next st st' → inv st → inv st' := by
+  intro st st' hnext ⟨hsafety, hinv_1, hinv_2⟩
+  rcases hnext with hsend | hrecv
+  {
+    rcases hsend with ⟨n, next, hpre, hpost⟩
+    simp only [inv, safety, inv_1, inv_2, hpost]
+    apply And.intro
+    { apply hsafety }
+    apply And.intro
+    {
+      rw [inv_1] at hinv_1
+      rintro S D N ⟨hp', hbtw'⟩
+      apply (hinv_1 S D N)
+      apply And.intro
+      {
+        simp at hp'; apply hp'
+        intro Hs Hd
+        -- suspect there is a contradiction with hpre and some of the `btw` axioms
+        simp [Hs, Hd] at hbtw'
+        rcases (hpre N) with ⟨hpre1, hpre2⟩
+        apply (st.between.btw_side _ _ _ hbtw')
+        apply hpre2
+        apply And.intro
+        {
+          intro Hn
+          rw [Hn] at hbtw'
+          have Hx : _ := st.between.btw_ring _ _ _ hbtw'
+          -- auto [hbtw', st.between.btw_ring, st.between.btw_trans, st.between.btw_side, st.between.btw_total]
+          sorry
+        }
+        {
+          sorry
+        }
+
+      }
+      { exact hbtw' }
+
+    }
+    { sorry }
+  }
+  { -- recv
+    sorry
+  }
+
+
+
 
 end Ring
--- LeanSts/State.lean
+++ LeanSts/State.lean
@@ -6,11 +6,19 @@ import LeanSts.Testing
 def updateFn [DecidableEq α] (f : α → β) (a : α) (b : β) : α → β :=
   λ (x : α) => if x = a then b else f x
 
-notation st"[ " a " ↦ " b " ]" => updateFn st a b
+def updateFn2 [DecidableEq α] [DecidableEq β] (f : α → β → γ)  (a : α) (b : β) (c : γ) : α → β → γ :=
+  λ (x : α) (y : β) => if x = a && y = b then c else f x y
+
+notation f"[ " a " ↦ " b " ]" => updateFn f a b
+notation f"[ " a " , " b " ↦ " c " ]" => updateFn2 f a b c
 
 @[simp] lemma updateFn_unfold [DecidableEq α] (f : α → β) (a : α) (b : β) (x : α) :
   (f[a ↦ b]) x = if x = a then b else f x := by
   simp only [updateFn]
+
+@[simp] lemma updateFn2_unfold  [DecidableEq α] [DecidableEq β] (f : α → β → γ)  (a : α) (b : β) (c : γ) :
+  (f[a , b ↦ c]) x y = if x = a && y = b then c else f x y := by
+  simp only [updateFn2]
 
 -- TODO: write a macro to define SampleableExt instances for Structures
 -- See https://github.com/verse-lab/lean-sts/commit/228605e7bb54c9f7cfec5d437f79c9607fb413fa
