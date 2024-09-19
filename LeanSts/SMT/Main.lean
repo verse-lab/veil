156786ff842f08caee51f23a4a7273fee7082bc8
George Pîrlea <george@pirlea.net>
2024-09-19 18:37:15 +0800

    WIP: Lean Auto queries

--- LeanSts/SMT/Main.lean
+++ LeanSts/SMT/Main.lean
@@ -300,6 +300,12 @@ partial def querySolver (goalQuery : String) (timeout : Nat) (forceSolver : Opti
 -- /-- Our own version of the `smt` & `auto` tactics. -/
 syntax (name := sauto) "sauto" smtHints smtTimeout : tactic
 
+/-- Converts `smtHints` into those understood by `lean-auto`. -/
+def parseAutoHints : TSyntax `smtHints → TacticM (TSyntax `Auto.hints)
+  | `(smtHints| [ $[$hs],* ]) => `(hints| [$[$hs:term],*])
+  | `(smtHints| ) => `(hints| [])
+  | _ => throwUnsupportedSyntax
+
 /- We use our own `parseTimeout` because the one in `lean-smt` has a
   hard-coded `5` as default if no timeout is specified. -/
 def parseTimeout : TSyntax `smtTimeout → TacticM Nat
@@ -307,11 +313,72 @@ def parseTimeout : TSyntax `smtTimeout → TacticM Nat
   | `(smtTimeout| ) => return (auto.smt.timeout.get (← getOptions))
   | _ => throwUnsupportedSyntax
 
+open Embedding.Lam Lean.Meta in
+/-- Alternative to `prepareQuery`, this invokes `lean-auto` to generate the
+  SMT query rather than `lean-smt`. The advantage is this is much faster
+  (see
+  [ufmg-smite#126](https://github.com/ufmg-smite/lean-smt/issues/126)),
+  but produces unreadable queries. -/
+def prepareAutoQuery (mv : MVarId) (hints : TSyntax `Auto.hints) : TacticM String := do
+  -- HACK: We operate on a cloned goal, and then reset it to the original.
+  let goal := (← mkFreshExprMVar (← mv.getType)).mvarId!
+  let (goalBinders, newGoal) ← goal.intros
+  let [nngoal] ← newGoal.apply (.const ``Classical.byContradiction [])
+    | throwError "prepareAutoQuery :: Unexpected result after applying Classical.byContradiction"
+  let (ngoal, absurd) ← MVarId.intro1 nngoal
+  replaceMainGoal [absurd]
+  withMainContext do
+    let (lemmas, inhFacts) ← collectAllLemmas hints #[] (goalBinders.push ngoal)
+    let query ← getQueryFromAuto lemmas inhFacts
+    -- IMPORTANT: Reset the goal to the original
+    replaceMainGoal [mv]
+    return query
+  where
+  -- based on `runAuto`
+  getQueryFromAuto
+    (lemmas : Array Lemma) (inhFacts : Array Lemma) : MetaM String := do
+    -- Simplify `ite`
+    let ite_simp_lem ← Lemma.ofConst ``Auto.Bool.ite_simp (.leaf "hw Auto.Bool.ite_simp")
+    let lemmas ← lemmas.mapM (fun lem => Lemma.rewriteUPolyRigid lem ite_simp_lem)
+    -- Simplify `decide`
+    let decide_simp_lem ← Lemma.ofConst ``Auto.Bool.decide_simp (.leaf "hw Auto.Bool.decide_simp")
+    let lemmas ← lemmas.mapM (fun lem => Lemma.rewriteUPolyRigid lem decide_simp_lem)
+    let afterReify (uvalids : Array UMonoFact) (uinhs : Array UMonoFact) (minds : Array (Array SimpleIndVal)) : LamReif.ReifM String := (do
+      let exportFacts ← LamReif.reifFacts uvalids
+      let mut exportFacts := exportFacts.map (Embedding.Lam.REntry.valid [])
+      let _ ← LamReif.reifInhabitations uinhs
+      let exportInds ← LamReif.reifMutInds minds
+      LamReif.printValuation
+      -- **Preprocessing in Verified Checker**
+      let (exportFacts', exportInds) ← LamReif.preprocess exportFacts exportInds
+      exportFacts := exportFacts'
+      getAutoQueryString exportFacts exportInds)
+    let (cmdString, _) ← Monomorphization.monomorphize lemmas inhFacts (@id (Reif.ReifM String) do
+      let s ← get
+      let u ← computeMaxLevel s.facts
+      (afterReify s.facts s.inhTys s.inds).run' {u := u})
+    return cmdString
+  getAutoQueryString (exportFacts : Array REntry) (exportInds : Array MutualIndInfo) : LamReif.ReifM String := do
+    let lamVarTy := (← LamReif.getVarVal).map Prod.snd
+    let lamEVarTy ← LamReif.getLamEVarTy
+    let exportLamTerms ← exportFacts.mapM (fun re => do
+      match re with
+      | .valid [] t => return t
+      | _ => throwError "runAuto :: Unexpected error")
+    let sni : SMT.SMTNamingInfo :=
+      {tyVal := (← LamReif.getTyVal), varVal := (← LamReif.getVarVal), lamEVarTy := (← LamReif.getLamEVarTy)}
+    let ((commands, _validFacts), _state) ← (lamFOL2SMT sni lamVarTy lamEVarTy exportLamTerms exportInds).run
+    let queryStr := String.intercalate "\n" (commands.toList.map toString)
+    return queryStr
+
+/-- `sauto` generates nice-looking SMT queries. -/
 @[tactic sauto] def evalSauto : Tactic := fun stx => withMainContext do
   let mv ← Tactic.getMainGoal
-  let hs ← Tactic.parseHints ⟨stx[1]⟩
   let timeout ← parseTimeout ⟨stx[2]⟩
-  let cmdString ← prepareQuery mv hs
+  -- let hs ← Tactic.parseHints ⟨stx[1]⟩
+  -- let cmdString ← prepareQuery mv hs
+  let hs ← parseAutoHints ⟨stx[1]⟩
+  let cmdString ← prepareAutoQuery mv hs
   let res ← querySolver cmdString timeout (retryOnFailure := true)
   match res with
   | .Sat _ => throwError "the goal is false"
