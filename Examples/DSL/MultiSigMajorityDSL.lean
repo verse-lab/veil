0daeb93ca148937e0ba1310f481c2084d904aea3
Elad Kinsbruner <eladkay@gmail.com>
2024-12-20 04:13:14 +0000

    [Case Studies] MultiSig-maj case study

--- Examples/DSL/MultiSigMajorityDSL.lean
+++ Examples/DSL/MultiSigMajorityDSL.lean
@@ -3,7 +3,7 @@ import Veil.DSL
 -- https://github.com/aman-goel/ivybench/blob/d2c9298fdd099001c71a34bc2e118db6f07d8404/multisig/ivy/multisig-all.ivy
 
 
-section MultiSigAll
+section MultiSigMaj
 open Classical
 
 type validator
@@ -11,6 +11,7 @@ type destination
 type value
 type deadline
 type signature
+type quorum
 
 relation holding : validator → Prop
 relation collect : validator → destination → value → deadline → Prop
@@ -24,9 +25,17 @@ relation cancelled : validator → destination → value → deadline → Prop
 
 relation expired : deadline → Prop
 
-#gen_state MultiSigAll
+relation member : signature → quorum → Prop
+relation chosenAt : quorum → validator → destination → value → deadline → Prop
+relation chosen : validator → destination → value → deadline → Prop
+
+#gen_state MultiSigMaj
 
 after_init {
+  require ∀ (q1 q2 : quorum), ∃ (s : signature), member s q1 ∧ member s q2; -- axiom [quorum_intersection]
+  require ∀ (q: quorum) (v: validator) (k: destination) (n: value) (d : deadline), chosenAt q v k n d ↔ (∀ (s: signature), member s q → sig v k n d s); -- axiom [chosenAt]
+  require ∀ (v: validator) (k: destination) (n: value) (d : deadline), chosen v k n d ↔ ∃ (q: quorum), chosenAt q v k n d; -- axiom [chosen]
+
   holding _ := True;
   collect _ _ _ _ := False;
 
@@ -48,6 +57,7 @@ action propose (n: validator) (k: destination) (v: value) (d: deadline) = {
 }
 
 action add_sig (n: validator) (k: destination) (v: value) (d: deadline) (s: signature) = {
+  require collect n k v d;
   require ¬ expired d;
   require sig_auth s;
   sig n k v d s := True
@@ -56,7 +66,8 @@ action add_sig (n: validator) (k: destination) (v: value) (d: deadline) (s: sign
 action pay (n: validator) (k: destination) (v: value) (d: deadline) = {
   require collect n k v d;
   require ¬ expired d;
-  require sig n k v d S;
+  require chosen n k v d;
+
   paid n k v d := True;
   holding n := True;
   collect n k v d := False
@@ -65,6 +76,7 @@ action pay (n: validator) (k: destination) (v: value) (d: deadline) = {
 action cancel (n: validator) (k: destination) (v: value) (d: deadline) = {
   require collect n k v d;
   require expired d;
+
   cancelled n k v d := True;
   holding n := True;
   collect n k v d := False
@@ -75,11 +87,11 @@ action expire (d: deadline) = {
 }
 
 safety [cancelled_after_deadline] cancelled N K V D → expired D
-safety [paid_if_enough_sigs] paid N K V D → (∀ s, sig N K V D s ∧ sig_auth s)
+safety [paid_if_enough_sigs] paid N K V D → (∃ q, ∀ s, (member s q) → (sig N K V D s ∧ sig_auth s))
 safety [paid] paid N K V D → proposed N K V D
 
 invariant [ic3po_global2] collect V1 D1 V2 D2 → proposed V1 D1 V2 D2
 invariant [ic3po_global3] sig V1 D1 V2 D2 S1 → sig_auth S1
-#gen_spec MultiSigAll
+#gen_spec MultiSigMaj
 
 #check_invariants
