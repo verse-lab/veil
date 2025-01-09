a4b6c1834acb43e2f0709c44ce11babad12f3430
volodeyka <vovaglad00@gmail.com>
2025-01-09 21:16:18 +0800

    refactor lang syntax

--- Veil/DSL/ActionLang.lean
+++ Veil/DSL/ActionLang.lean
@@ -25,7 +25,7 @@ inductive Lang.{u} : Type u → Type (u + 1) where
   | require (rq : σ -> Prop) : Lang PUnit
   | bind    {ρ ρ' : Type u} (act : Lang ρ') (act' : ρ' -> Lang ρ) : Lang ρ
   -- τ is a first order type, so it can be quantified in FOL
-  | fresh   (τ : Type u) {ρ : Type u} : Lang ρ
+  | fresh   (τ : Type u) : Lang τ
   /-- Deterministic changes to the state, although mostly used for assignments. All
       capital variables will be quantified. -/
   | det     {ρ' : Type u} (act : σ -> σ × ρ') : Lang ρ'
@@ -62,7 +62,6 @@ inductive Lang.{u} : Type u → Type (u + 1) where
   -- This corresponds to the semantics in Ivy and matches the intuition that
   -- a call to an action is morally equivalent to inlining that action.
   | Lang.nondet act       => fun s => ∃ s' ret, act s (s', ret) ∧ post ret s'
---| Lang.ensure p         => fun s => ∃ s' ret, p s s' ∧ post ret s'
   -- the meaning of `ite` depends on which branch is taken
   | Lang.ite cnd thn els  => fun s => if cnd s then wlp post thn s else wlp post els s
   | Lang.iteSome cnd thn els  =>
@@ -78,45 +77,50 @@ declare_syntax_cat left_arrow
 syntax "<-" : left_arrow
 syntax "←" : left_arrow
 
+/-- Sequense of [lang] commands -/
 declare_syntax_cat langSeq
+/-- Let binding or a single [lang] statement -/
 declare_syntax_cat langElem
+/-- Single [lang] statement -/
 declare_syntax_cat lang
 
 syntax lang : langElem
+/- [lang] let binging -/
 syntax "let" ident left_arrow lang : langElem
 
+/-- Sequense of [lang] commands -/
 syntax sepByIndentSemicolon(langElem) : langSeq
 
--- syntax lang ";" colGe lang : lang
--- syntax lang : lang
+/-- [skip] statement which does nothing -/
 syntax "skip"              : lang
 /-- Non-deterministic value. -/
 syntax (name := nondetVal) "*" : lang
+/-- `require s` checks if `s` is true on the current state -/
 syntax "require" term      : lang
-syntax "do" term           : lang
+/-- `fresh [Type]?` creates a fresh variable of type `Type`, or of an implicit type if `Type` is ommited -/
+syntax "fresh" term ? : lang
+/-- `return t` returns `t`  -/
+syntax "return" term : lang
+syntax term : lang
 
 declare_syntax_cat some_if
 syntax ident "where" : some_if
-syntax (priority := high) "if" (some_if)? term:max "{" langSeq "}" "else\n" "{" langSeq "}" : lang
-syntax (priority := low) "if" (some_if)? term:max "{" langSeq "}" : lang
+syntax (priority := high) "if " (some_if)? term " then" langSeq colGe "else\n" langSeq : lang
+syntax (priority := low) "if " (some_if)? term " then" langSeq : lang
+
 /-- intermediate syntax for assigment, e.g. `pending := pending[n, s ↦ true]` -/
 syntax Term.structInstLVal ":=" term    : lang
 /-- syntax for assigment, e.g. `pending n s := true` -/
 syntax Term.structInstLVal (term:max)+ ":=" (term <|> nondetVal)    : lang
 syntax term (":" term)? left_arrow lang "in" langSeq : lang
-syntax "fresh" ident ":" term "in" langSeq : lang
-syntax "return" term : lang
-syntax "call" term : lang
 
 declare_syntax_cat unchanged
 syntax "with unchanged" "[" ident,* "]" : unchanged
-
 syntax "ensure" rcasesPat  "," term unchanged ? : lang
 syntax "ensure" term unchanged ? : lang
 syntax "[unchanged|" ident* "]" : term
 
-/-- Syntax to trigger the expantion into a code which may
-    depend on the prestate -/
+/-- Syntax to trigger the expantion into a code -/
 syntax "[langSeq|" langSeq "]" : term
 syntax "[lang|" lang "]" : term
 
@@ -138,16 +142,6 @@ def closeCapitals (s : Term) : MacroM Term :=
   let caps := getCapitals s
   `(forall $[$caps]*, $s)
 
-/-- Alternate version of `Term.adaptExpander`, which gives an extra
-`TSyntax term` argument to `exp`. We use this to provide type
-annotations to the state type in the expansion of the imperative DSL.
-(Without this, when type inference fails, the user sees a cryptic
-"expected structure" error.) For action return values, we rely on Lean's
-type inference. -/
-def adaptExpander' (exp : Syntax → TSyntax `term → TermElabM Syntax) : Syntax → TSyntax `term → TermElabM Expr := fun stx expectedType => do
-  let stx' ← exp stx expectedType
-  withMacroExpansion stx stx' <| elabTerm stx' .none
-
 /-- Throw an error if the field (which we're trying to assign to) was
 declared immutable. FIXME: make sure elaboration aborts? -/
 def throwIfImmutable (lhs : TSyntax `Lean.Parser.Term.structInstLVal) : TermElabM Unit := do
@@ -166,44 +160,16 @@ def throwIfImmutable (lhs : TSyntax `Lean.Parser.Term.structInstLVal) : TermElab
     Instead of writing `fun st => Pred` this command will pattern match over
     `st` making all its fields accessible for `Pred` -/
 macro "funcases" t:term : term => `(term| by intros st; unhygienic cases st; exact $t)
-
-open Tactic in
-/-- ```with_rename_old t``` runs tactic `t` and if it introduces any names with `_1` suffix,
-    changes this suffix to `_old` -/
-elab "with_rename_old" t:tactic : tactic => withMainContext do
-  let hyps <- getLCtx
-  withMainContext $ evalTactic t
-  withMainContext do
-    let hypNews <- getLCtx
-    for hyp in hypNews.decls.toArray.reverse do
-      if hyp.isSome then
-        let hyp := hyp.get!
-        unless hyps.findFromUserName? hyp.userName |> Option.isSome do
-          let nms := (hyp.userName.toString.splitOn "_")
-          let new_name := String.intercalate "_" nms.dropLast ++ "_old" |>.toName
-          evalTactic $ <- `(tactic| (revert $(mkIdent hyp.userName):ident; intros $(mkIdent new_name):ident))
-
-
-/-- This is used wherener we want to define a predicate over a state
-    which should not depend on the state (for instance in `after_init`). -/
-macro "funclear" t:term : term => `(term| by intros st; clear st; exact $t)
-
-macro_rules
-  | `([unchanged| $id:ident]) =>
-    let id_old := id.getId.toString ++ "_old" |>.toName
-    `($id = $(mkIdent id_old))
-  | `([unchanged| $id $ids*]) => `([unchanged| $id] ∧ [unchanged| $ids*])
-  | `([unchanged|]) => `(True)
-
-/-- This is used when we want to define a predicate over a state
-    which should not depend on the pre-state. -/
-macro "funclear" t:term : term => `(term| by intros st; clear st; exact $t)
-
+macro "funcases" id:ident t:term : term => `(term| by unhygienic cases $id:ident; exact $t)
 
 /-====== Macro expansion for [lang] ======-/
 
+/-- In some cases, during the macro expansion, we need to annotate certain
+    arguments with the state type. To get the state type, we need an access to an
+    eviorment with we don't have at the macro-expansion stage. To overcome this, we
+    introduce the following notation, witch gets resolved to a current state type
+    during the elaboration stage  -/
 syntax "[State]" : term
-
 elab_rules : term
   | `([State]) => do
     let stateTp := (← localSpecCtx.get).spec.stateStx
@@ -229,15 +195,92 @@ macro_rules
     `(@Lang.bind _ _ _ [lang|$l] (fun $id => [langSeq|$[$ls]*]))
 
 
-/- Simple statements: `require`, `return` -/
+/- Simple single statements: `require`, `return`, `fresh` and `call` -/
 
 macro_rules
-  | `([lang|return $t:term]) => `(@Lang.ret _ _ (by unhygienic cases $(mkIdent `st):ident; exact $t))
-  | `([lang|call $t:term]) => `(@Lang.nondet _ _ (by unhygienic cases $(mkIdent `st):ident; exact $t))
+  | `([lang|return $t:term]) => `(@Lang.ret _ _ (funcases $(mkIdent `st):ident $t))
+  | `([lang|$t:term])   => `(@Lang.nondet _ _ (funcases $(mkIdent `st):ident $t))
+  | `([lang|require $t:term]) => do
+    -- universally close term [t] under free variables starting with a capital letter
+    let t' <- closeCapitals t
+    withRef t $ `(@Lang.require _ (funcases ($t' : Prop) : _ -> Prop))
+    -- infer the type of a fresh variable, if it is not given
+  | `([lang|fresh]) => `([lang|fresh _])
+  | `([lang|fresh $t:term]) => `(@Lang.fresh _ $t)
 
+/- If-then-else -/
 
+macro_rules
+  | `([lang|if $some_if ? $cnd:term then $thn:langSeq]) => `([lang|if $some_if ? $cnd then $thn else skip])
+  | `([lang|if $cnd:term then $thn:langSeq else $els:langSeq]) => do
+    let cnd' <- closeCapitals cnd
+    -- condition might depend on the state as well
+    let cnd <- withRef cnd `(funcases ($cnd' : Bool))
+    `(@Lang.ite _ _ ($cnd: term) [langSeq|$thn] [langSeq|$els])
+  | `([lang|if $x:ident where $cnd:term then $thn:langSeq else $els:langSeq]) => do
+    let cnd' <- closeCapitals cnd
+    -- condition might depend on the state as well
+    let cnd <- withRef cnd `(funcases ($cnd' : Bool))
+    `(@Lang.iteSome _ _ _ (fun $x:ident => $cnd:term) (fun $x => [langSeq|$thn]) [langSeq|$els])
+
+/- Assignments -/
 
 elab_rules : term
+  -- expansion of the intermediate syntax for assigment
+  -- for instance `pending := pending[n, s ↦ true]` will get
+  -- expanded to `Lang.det (fun st => { st with pending := st.pending[n, s ↦ true] })`
+  | `([lang| $id:structInstLVal := $t:term]) => do
+    throwIfImmutable id
+    elabTerm (<- `(@Lang.det _ _ (fun (st : [State]) =>
+      ({ st with $id := (funcases st $t)}, ())))) none
+  -- non-deterministic assignment
+  | `([lang| $id:structInstLVal $ts: term * := *]) => do
+    throwIfImmutable id
+    elabTerm (<- `(@Lang.fresh _ _ _ (fun v => @Lang.nondet _ _ _ (fun (st : [State]) =>
+      ({ st with $id := (funcases st ($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ v ]))}, ()))))) none
+  -- expansion of the actual syntax for assignment
+  -- for instance `pending n s := true` will get
+  -- expanded to `pending := pending[n, s ↦ true]`
+  | `([lang| $id:structInstLVal $ts: term * := $t:term]) => do
+    throwIfImmutable id
+    let stx <- withRef id `($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ $t:term ])
+    elabTerm (<- `([lang| $id:structInstLVal := $stx])) none
+
+/- Ensures statement -/
+
+open Tactic in
+/-- ```with_rename_old t``` runs tactic `t` and if it introduces any names with `_1` suffix,
+    changes this suffix to `_old` -/
+elab "with_rename_old" t:tactic : tactic => withMainContext do
+  let hyps <- getLCtx
+  withMainContext $ evalTactic t
+  withMainContext do
+    let hypNews <- getLCtx
+    for hyp in hypNews.decls.toArray.reverse do
+      if hyp.isSome then
+        let hyp := hyp.get!
+        unless hyps.findFromUserName? hyp.userName |> Option.isSome do
+          let nms := (hyp.userName.toString.splitOn "_")
+          let new_name := String.intercalate "_" nms.dropLast ++ "_old" |>.toName
+          evalTactic $ <- `(tactic| (revert $(mkIdent hyp.userName):ident; intros $(mkIdent new_name):ident))
+
+/- Expanding unchanged statement -/
+macro_rules
+  | `([unchanged| $id:ident]) =>
+    let id_old := id.getId.toString ++ "_old" |>.toName
+    `($id = $(mkIdent id_old))
+  | `([unchanged| $id $ids*]) => `([unchanged| $id] ∧ [unchanged| $ids*])
+  | `([unchanged|]) => `(True)
+
+/- One can omit the result variable from [ensures] -/
+macro_rules
+  | `([lang|ensure $t:term $un:unchanged ?] ) => `([lang|ensure (_ : Unit), $t:term $un:unchanged ?])
+
+elab_rules : term
+  /- if the list of unchanged state fileds is omitted, we can restore it by
+     checking witch of them are mentioned in the [ensures] body. By default
+     we assume that if the state filed is not mentioned, then it is left
+     unchanged  -/
   | `([lang|ensure $r, $t:term]) => do
     let fields : Array Name := (<- localSpecCtx.get).spec.signature.map (·.name)
     let mut unchangedFields := #[]
@@ -252,78 +295,5 @@ elab_rules : term
          unhygienic cases st';
          with_rename_old unhygienic cases st;
          exact $t ∧ [unchanged|$ids*]))) none
-  | `([lang| $id:structInstLVal $ts: term * := *]) => do
-    throwIfImmutable id
-    let stateTp := (← localSpecCtx.get).spec.stateStx
-    elabTerm (<- `(@Lang.fresh _ _ _ (fun v => @Lang.nondet _ _ _ (fun (st : $stateTp) =>
-      ({ st with $id := (by unhygienic cases st; exact ($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ v ]))}, ()))))) none
-  -- non-deterministic assignment
-    -- expansion of the intermediate syntax for assignment
-    -- for instance `pending := pending[n, s ↦ true]` will get
-    -- expanded to `Lang.det (fun st => { st with pending := st.pending[n, s ↦ true] })`
-  | `([lang| $id:structInstLVal := $t:term]) => do
-    throwIfImmutable id
-    let stateTp := (← localSpecCtx.get).spec.stateStx
-    elabTerm (<- `(@Lang.det _ _ (fun (st : $stateTp) =>
-      ({ st with $id := (by unhygienic cases st; exact $t)}, ())))) none
-  -- expansion of the actual syntax for assignment
-  -- for instance `pending n s := true` will get
-  -- expanded to `pending := pending[n, s ↦ true]`
-  | `([lang| $id:structInstLVal $ts: term * := $t:term]) => do
-    throwIfImmutable id
-    let stx <- withRef id `($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ $t:term ])
-    elabTerm (<- `([lang| $id:structInstLVal := $stx])) none
-
-macro_rules
-  -- | `([lang|skip]) => `(@Lang.det _ _ (fun st => (st, ())))
-  -- | `([langSeq| ]) => `(@Lang.det _ _ (fun st => (st, ())))
-  | `([lang|require $t:term]) => do
-    let t' <- closeCapitals t
-    withRef t $
-      -- require a proposition on the state
-     `(@Lang.require _ (funcases ($t' : Prop) : _ -> Prop))
-  --
-  | `([lang|if $some_if ? $cnd:term { $thn:langSeq }]) => `([lang|if $some_if ? $cnd { $thn } else { skip }])
-  | `([lang|if $cnd:term { $thn:langSeq } else { $els:langSeq }]) => do
-    let cnd' <- closeCapitals cnd
-    -- condition might depend on the state as well
-    let cnd <- withRef cnd `(funcases ($cnd' : Bool))
-    `(@Lang.ite _ _ ($cnd: term) [langSeq|$thn] [langSeq|$els])
-  | `([lang|if $x:ident where $cnd:term { $thn:langSeq } else { $els:langSeq }]) => do
-    let cnd' <- closeCapitals cnd
-    -- condition might depend on the state as well
-    let cnd <- withRef cnd `(funcases ($cnd' : Bool))
-    `(@Lang.iteSome _ _ _ (fun $x:ident => $cnd:term) (fun $x => [langSeq|$thn]) [langSeq|$els])
-  --
-  | `([lang|ensure $t:term $un:unchanged ?] ) =>
-    `([lang|ensure (_ : Unit), $t:term $un:unchanged ?])
-  | `([lang| do $t:term ]) => `(@Lang.det _ _ $t)
-    -- expansion of the intermediate syntax for assigment
-    -- for instance `pending := pending[n, s ↦ true]` will get
-    -- expanded to `Lang.det (fun st => { st with pending := st.pending[n, s ↦ true] })`
-  | `([lang| $id:structInstLVal := $t:term ]) => do
-    `(@Lang.det _ _ (fun st =>
-      ({ st with $id := (by unhygienic cases st; exact $t)}, ())))
-  -- for instance `pending n s := *` will get
-  -- | `([lang| $id:structInstLVal $ts: term * := * ]) => do
-  --   `(@Lang.nondet _ _ (fun st (st', ()) =>
-  --     (∃ v, st' = { st with $id := (by unhygienic cases st; exact ($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ v ]))})))
-  | `([lang| $id:structInstLVal $ts: term * := * ]) => do
-    `(@Lang.fresh _ _ _ (fun v => @Lang.nondet _ _ _ (fun st =>
-      ({ st with $id := (by unhygienic cases st; exact ($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ v ]))}, ()))))
-  --   -- expansion of the actual syntax for assigment
-    -- for instance `pending n s := true` will get
-    -- expanded to `pending := pending[n, s ↦ true]`
-  | `([lang| $id:structInstLVal $ts: term * := $t:term ]) => do
-    let stx <- withRef id `($(⟨id.raw.getHead?.get!⟩)[ $[$ts],* ↦ $t:term ])
-    `([lang| $id:structInstLVal := $stx])
-  -- NOTE: the following two cases describe the same construct
-  -- there's probably a way to unify them
-  | `([lang| $id:term $_:left_arrow $l1:lang in $l2:langSeq]) => do
-      `(@Lang.bind _ _ _ [lang|$l1] (fun $id => [langSeq|$l2]))
-  | `([lang| $id:term : $t:term $_:left_arrow $l1:lang in $l2:langSeq]) => do
-      `(@Lang.bind _ _ _ [lang|$l1] (fun ($id : $t) => [langSeq|$l2]))
-  | `([lang|fresh $id:ident : $t in $l2:langSeq]) =>
-      `(@Lang.fresh _ _ _ (fun $id : $t => [langSeq|$l2]))
 
 end Lang
