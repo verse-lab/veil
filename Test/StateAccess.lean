ce2c5c1c732140bb9a28c9f4ea2b42b0aa294c5c
George Pîrlea <george@pirlea.net>
2025-01-15 14:23:10 +0800

    New test: broken pattern binding
    The fix should be in `hasRHS?`

---
+++ Test/StateAccess.lean
@@ -0,0 +1,29 @@
+import Veil.DSL
+import Veil.TestUtil
+
+open Classical
+set_option linter.unusedVariables false
+
+section Test
+type block
+type queue
+
+individual x : Prop
+individual b : block
+relation r : block -> block -> Prop
+
+#gen_state Test
+
+action with_block (b : block) = {
+  let b' ← fresh block
+  return (b, b')
+}
+
+action test = {
+  let mut (x, y) := (5, 7)
+}
+
+#guard_msgs in
+action double_bind (r : Int) = {
+    let (b, b') ← with_block b
+ }
