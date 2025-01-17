4d6b40c52394b81dbf1b9ea7b19d20e43c34847a
George Pîrlea <george@pirlea.net>
2025-01-17 12:15:30 +0800

    Add test: recursive actions should fail (behaviour OK; test broken)

---
+++ Test/NoRecursiveAction.lean
@@ -0,0 +1,22 @@
+import Veil.DSL
+import Veil.TestUtil
+
+open Classical
+set_option linter.unusedVariables.analyzeTactics true
+set_option sauto.smt.translator "lean-smt"
+
+section Test
+type node
+
+relation r : node -> Nat -> Prop
+individual n : node
+function f : Nat -> Nat
+
+#gen_state Test
+
+-- FIXME BROKEN test: we want this to throw an error, but don't really care
+-- what it is
+#guard_msgs(drop all) in
+action foo = {
+  foo
+}
