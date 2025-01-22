0dad1ae4bdc99b086ab921dc60708edf84c24cb5
Elad Kinsbruner <kinsbruner@campus.technion.ac.il>
2025-01-22 14:43:58 +0800

    [Test] Test for action in `#check_invariants!`

--- Test/CheckBangQuestion.lean
+++ Test/CheckBangQuestion.lean
@@ -10,6 +10,7 @@ instantiate btwn : Between node
 
 open Between TotalOrder
 
+individual indv : Bool
 relation leader : node → Prop
 relation pending : node → node → Prop
 
@@ -18,6 +19,7 @@ relation pending : node → node → Prop
 after_init {
   leader N := False
   pending M N := False
+  indv := False
 }
 
 action send (n next : node) = {
@@ -36,17 +38,40 @@ action recv (sender n next : node) = {
       pending sender next := True
 }
 
+action ruin_inv = {
+  indv := False
+}
+
 safety [single_leader] leader L → le N L
 invariant pending S D ∧ btw S N D → le N S
-invariant [false_inv] False
+invariant [indv_true] indv
 
 #gen_spec
 
 /-- info:
-@[invProof] theorem init_Ring.false_inv :
+@[invProof]
+  theorem init_Ring.indv_true :
       ∀ (st : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
-          (@System node node_dec node_ne tot btwn).init st → (@Ring.false_inv node node_dec node_ne tot btwn) st :=
+          (@System node node_dec node_ne tot btwn).init st → (@Ring.indv_true node node_dec node_ne tot btwn) st :=
+    by (unhygienic intros); exact sorry
+
+  @[invProof]
+  theorem Ring.indv_true_ruin_inv :
+      ∀ (st st' : @State node),
+        (@System node node_dec node_ne tot btwn).assumptions st →
+          (@System node node_dec node_ne tot btwn).inv st →
+            (@Ring.ruin_inv.tr node node_dec node_ne tot btwn) st st' →
+              (@indv_true node node_dec node_ne tot btwn) st' :=
+    by (unhygienic intros); exact sorry
+
+  @[invProof]
+  theorem Ring.single_leader_recv :
+      ∀ (st st' : @State node),
+        (@System node node_dec node_ne tot btwn).assumptions st →
+          (@System node node_dec node_ne tot btwn).inv st →
+            (@Ring.recv.tr node node_dec node_ne tot btwn) st st' →
+              (@single_leader_recv node node_dec node_ne tot btwn) st' :=
     by (unhygienic intros); exact sorry
 -/
 #guard_msgs(whitespace := lax) in
