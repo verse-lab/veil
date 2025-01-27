83c83ec8a6dca54b8f1862cdd80c0f36cfe30da6
volodeyka <vovaglad00@gmail.com>
2025-01-28 00:40:06 +0800

    [Case study] fix Vertical paxos

--- Examples/DSL/VerticalPaxosFirstOrder.lean
+++ Examples/DSL/VerticalPaxosFirstOrder.lean
@@ -49,7 +49,7 @@ after_init {
 
 -- master action
 action configure_round (r : round) (c : config) = {
-  require ¬ configure_round_msg r c
+  require ∀ C, ¬ configure_round_msg r C
   require tot.le master_complete r
   require complete_of c = master_complete
 
@@ -90,11 +90,13 @@ action propose (r : round) (c : config) (cr : round) = {
   require ∀ R N, tot.le cr R ∧ ¬ tot.le r R ∧ member N (quorum_of_round R) → ∃ v, join_ack_msg N r R v
   let mut v : value ← fresh
   let maxr : round ← fresh
-  require (v = none ∧ ∀ n mAXR v, ¬ (tot.le cr mAXR ∧ ¬ tot.le r maxr ∧ member n (quorum_of_round mAXR) ∧ join_ack_msg n r mAXR v ∧ v ≠ none)) ∨
-          (v ≠ none ∧ (∃ n, tot.le cr maxr ∧ ¬ tot.le r maxr ∧ member n (quorum_of_round maxr) ∧ join_ack_msg n r maxr v ∧ v ≠ none) ∧
-          (∀ n mAXR v, (tot.le cr mAXR ∧ ¬ tot.le r mAXR ∧ member n (quorum_of_round mAXR) ∧ join_ack_msg n r mAXR v ∧ v ≠ none) → tot.le mAXR maxr))
-
-
+  assume ((v = none ∧ forall N MAXR V,
+                      ¬ (tot.le cr MAXR ∧ ¬tot.le r MAXR ∧ member N (quorum_of_round MAXR)) ∧ join_ack_msg N r MAXR V ∧ V ≠ none) ∨
+                      (v ≠ none ∧
+                      (exists N:node, tot.le cr maxr ∧ ¬tot.le r maxr ∧ member N (quorum_of_round maxr) ∧ join_ack_msg N r maxr v ∧ v ≠ none) ∧
+                      (forall N MAXR V,
+                       (tot.le cr MAXR  ∧ ¬tot.le r MAXR ∧ member N (quorum_of_round MAXR) ∧ join_ack_msg N r MAXR V ∧ V ≠ none) -> tot.le MAXR maxr)
+                    ));
   if v = none then
     v := *
     require v ≠ none
@@ -104,7 +106,7 @@ action propose (r : round) (c : config) (cr : round) = {
 
 action cast_vote (n : node) (v : value) (r : round) = {
   require v ≠ none
-  require ¬ (∃ (r' : round) (rp : round) (v' : value), join_ack_msg n r' rp v ∧ ¬ tot.le r' r)
+  require ¬ (∃ (r' : round) (rp : round) (v' : value), join_ack_msg n r' rp v' ∧ ¬ tot.le r' r)
   require proposal r v
   vote n r v := True
 }
@@ -120,6 +122,8 @@ action decide (n : node) (r : round) (c : config) (v : value) (q : quorum) = {
 
 safety [coherence] (decision N1 R1 V1 ∧ decision N2 R2 V2) → V1 = V2
 
+invariant [proposal_unique] proposal R V1 ∧ proposal R V2 → V1 = V2
+
 invariant [unique_config] configure_round_msg R C1 ∧ configure_round_msg R C2 → C1 = C2
 
 invariant [property1_one_a] one_a R RP → ¬ tot.le R RP
@@ -129,14 +133,14 @@ invariant [only_vote_proposed] vote N R V → proposal R V
 
 invariant [propose_if_lower_configured] proposal R2 V ∧ tot.le R1 R2 → ∃ c, configure_round_msg R1 c
 
-invariant [master_complete_zero_or_holds] (R2 = master_complete ∨ (configure_round_msg R3 C ∧ complete_of C = R2)) ∧ tot.le R2 R1 → complete_msg R2
+invariant [master_complete_zero_or_holds] (R2 = master_complete ∨ (configure_round_msg R3 C ∧ complete_of C = R2)) ∧ ¬ tot.le R2 R1 → complete_msg R2
 invariant [complete_prev_configured] complete_msg R2 ∧ tot.le R1 R2 → ∃ c, configure_round_msg R1 c
 
 -- conjecture forall R:round, V:value. (exists N:node. decision(N,R,V)) -> exists C:config, Q:quorum. configure_round_msg(R,C) & quorumin(Q,C) & (forall N:node. member(N, Q) -> vote(N,R,V))
 invariant [decisions_from_quorums] (∃ n, decision n R V) → ∃ c q, configure_round_msg R c ∧ quorumin q c ∧ (∀ n, member n q → vote n R V)
 
 invariant [choosable_proposal] ¬ tot.le R2 R1 ∧ proposal R2 V2 ∧ V1 ≠ V2 ∧
-  configure_round_msg R1 C ∧ quorumin Q C → ∃ n r3 rp v, member n Q ∧ ¬ tot.le r3 R1 ∧ join_ack_msg n R3 rp v ∧ ¬ vote n R1 V1
+  configure_round_msg R1 C ∧ quorumin Q C → ∃ n r3 rp v, member n Q ∧ ¬ tot.le r3 R1 ∧ join_ack_msg n r3 rp v ∧ ¬ vote n R1 V1
 
 invariant [configure_round_msg] configure_round_msg R C → tot.le (complete_of C) R ∧ tot.le (complete_of C) master_complete
 
@@ -149,13 +153,13 @@ invariant [none_property2] ¬vote N R none
 invariant [none_property3] ¬decision N R none
 
 invariant [join_ack_msg_property1] join_ack_msg N R RP V → one_a R RP
-invariant [join_ack_msg_property2] join_ack_msg N R RP V → ¬ vote N RP V
+invariant [join_ack_msg_property2] join_ack_msg N R RP none → ¬ vote N RP V
 invariant [join_ack_msg_property3] join_ack_msg N R RP V ∧ V ≠ none → vote N RP V
 
 #gen_spec
 
 set_option sauto.smt.solver "cvc5"  -- for its determinism
 
-#time #check_invariants_wlp!
+#time #check_invariants_wlp
 
 end VerticalPaxosFirstOrder
