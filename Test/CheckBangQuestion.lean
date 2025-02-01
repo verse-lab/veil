32af82049ee5f2b41ac3c0f65758639c7416beff
zqy1018 <zqy1018@hotmail.com>
2025-02-01 12:53:22 +0800

    fix tests

--- Test/CheckBangQuestion.lean
+++ Test/CheckBangQuestion.lean
@@ -68,14 +68,14 @@ The following set of actions must preserve the invariant:
     indv_true ... ❌
 ---
 info: @[invProof]
-  theorem init_Ring.indv_true :
+  theorem init_indv_true :
       ∀ (st : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).init st → (@Ring.indv_true node node_dec node_ne tot btwn) st :=
     by (unhygienic intros); exact sorry
   ⏎
   @[invProof]
-  theorem ruin_inv_Ring.indv_true :
+  theorem ruin_inv_indv_true :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
@@ -84,7 +84,7 @@ info: @[invProof]
     by (unhygienic intros); exact sorry
   ⏎
   @[invProof]
-  theorem recv_Ring.single_leader :
+  theorem recv_single_leader :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
--- Test/CheckQuestion.lean
+++ Test/CheckQuestion.lean
@@ -43,14 +43,14 @@ invariant pending L L → le N L
 #gen_spec
 
 /-- info:
-@[invProof] theorem init_Ring.single_leader :
+@[invProof] theorem init_single_leader :
       ∀ (st : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).init st →
             (@Ring.single_leader node node_dec node_ne tot btwn) st :=
     by (unhygienic intros); solve_clause[initSimp]
 
-@[invProof] theorem Ring.send_Ring.single_leader :
+@[invProof] theorem Ring.send_single_leader :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
@@ -58,7 +58,7 @@ invariant pending L L → le N L
               (@Ring.single_leader node node_dec node_ne tot btwn) st' :=
     by (unhygienic intros); solve_clause[Ring.send.tr]
 
-@[invProof] theorem Ring.recv_Ring.single_leader :
+@[invProof] theorem Ring.recv_single_leader :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
@@ -71,7 +71,7 @@ invariant pending L L → le N L
 
 /-- info:
 @[invProof]
-  theorem Ring.recv_Ring.single_leader :
+  theorem Ring.recv_single_leader :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
@@ -79,7 +79,7 @@ invariant pending L L → le N L
               (@Ring.single_leader node node_dec node_ne tot btwn) st' :=
     by (unhygienic intros); solve_clause[Ring.recv.tr]
 
-@[invProof] theorem Ring.recv_Ring.inv_1 :
+@[invProof] theorem Ring.recv_inv_1 :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
@@ -87,7 +87,7 @@ invariant pending L L → le N L
               (@Ring.inv_1 node node_dec node_ne tot btwn) st' :=
     by (unhygienic intros); solve_clause[Ring.recv.tr]
 
-@[invProof] theorem Ring.recv_Ring.inv_2 :
+@[invProof] theorem Ring.recv_inv_2 :
       ∀ (st st' : @State node),
         (@System node node_dec node_ne tot btwn).assumptions st →
           (@System node node_dec node_ne tot btwn).inv st →
