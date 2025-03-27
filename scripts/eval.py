c8e8586be4496bbf778c800692f4d28db4ca22be
George Pîrlea <george@pirlea.net>
2025-03-27 16:25:16 +0800

    eval: produce normalized plot

--- scripts/eval.py
+++ scripts/eval.py
@@ -85,7 +85,7 @@ def run_dir(dir: str) -> dict[str, dict[str, float]]:
                 print(f"Failed to run {filename}: {e}", file=sys.stderr)
     return ret
 
-def create_graph(res : dict[str, dict[str, float]], output_file : str):
+def create_graphs(res : dict[str, dict[str, float]], output_file : str):
     categories = {it: it.split("/")[-1].split(".")[0] for it in res.keys()}
     res  = {k: [v["simplification_time"], v["translation_time"], v["solving_time"], v["total_time"], v["total_ivy_time"]] for k, v in res.items()}
     simp_times = np.array([res[it][0] for it in categories]) # Bottom part of stacked bars
@@ -95,62 +95,138 @@ def create_graph(res : dict[str, dict[str, float]], output_file : str):
     ivy_times = np.array([res[it][4] for it in categories]) # Separate bar for Ivy
     x = np.arange(len(categories))  # X-axis positions
 
-    # Plot 1: raw times
-    fig, (ax_high, ax_low) = plt.subplots(2, 1, sharex=True, figsize=(8, 6), gridspec_kw={'height_ratios': [2, 1], 'hspace': 0.05})
-    ax_low.set_ylim(0, 30)  # Normal range
-    ax_high.set_ylim(200, 600)  # Normal range
+    def create_raw_graph(output_file : str):
+        fig, (ax_high, ax_low) = plt.subplots(2, 1, sharex=True, figsize=(8, 6), gridspec_kw={'height_ratios': [2, 1], 'hspace': 0.05})
+        ax_low.set_ylim(0, 30)  # Normal range
+        ax_high.set_ylim(200, 600)  # Normal range
 
-    ax_low.bar(x - 0.2, simp_times, width=0.4, label='Simplification Time', color='blue')
-    ax_low.bar(x - 0.2, trans_times, width=0.4, bottom=simp_times, label='Translation Time', color='lightblue')
-    ax_low.bar(x - 0.2, solving_times, width=0.4, bottom=simp_times+trans_times,label='Solver Time', color='green')
-    ax_low.bar(x + 0.2, ivy_times, width=0.4, label='Ivy Runtime', color='red')
+        ax_low.bar(x - 0.2, simp_times, width=0.4, label='Simplification Time', color='blue')
+        ax_low.bar(x - 0.2, trans_times, width=0.4, bottom=simp_times, label='Translation Time', color='lightblue')
+        ax_low.bar(x - 0.2, solving_times, width=0.4, bottom=simp_times+trans_times,label='Solver Time', color='green')
+        ax_low.bar(x + 0.2, ivy_times, width=0.4, label='Ivy Runtime', color='red')
 
-    ax_high.bar(x - 0.2, simp_times, width=0.4, label='Simplification Time', color='blue')
-    ax_high.bar(x - 0.2, trans_times, width=0.4, bottom=simp_times, label='Translation Time', color='lightblue')
-    ax_high.bar(x - 0.2, solving_times, width=0.4, bottom=simp_times+trans_times,label='Solver Time', color='green')
-    ax_high.bar(x + 0.2, ivy_times, width=0.4, label='Ivy Runtime', color='red')
+        ax_high.bar(x - 0.2, simp_times, width=0.4, label='Simplification Time', color='blue')
+        ax_high.bar(x - 0.2, trans_times, width=0.4, bottom=simp_times, label='Translation Time', color='lightblue')
+        ax_high.bar(x - 0.2, solving_times, width=0.4, bottom=simp_times+trans_times,label='Solver Time', color='green')
+        ax_high.bar(x + 0.2, ivy_times, width=0.4, label='Ivy Runtime', color='red')
 
-    # Add text for time on top of the bars
-    for it in range(len(categories)):
-        # Veil runtimes
-        time = total_times[it]
-        text = str(time)
-        text = text if len(text) < 5 else text[:4]
-        if time + 1 <= 30:
-            ax_low.text(x[it] - 0.2, time + 1, text, fontsize=11, color='black', ha='center', rotation=90)
-        ax_high.text(x[it] - 0.2, 2 + time, text, fontsize=11, color='black', ha='center', rotation=90)
+        # Add text for time on top of the bars
+        for it in range(len(categories)):
+            # Veil runtimes
+            time = total_times[it]
+            text = str(time)
+            text = text if len(text) < 5 else text[:4]
+            if time + 1 <= 30:
+                ax_low.text(x[it] - 0.2, time + 1, text, fontsize=11, color='black', ha='center', rotation=90)
+            ax_high.text(x[it] - 0.2, 2 + time, text, fontsize=11, color='black', ha='center', rotation=90)
 
-        # Ivy runtimes
-        time = ivy_times[it]
-        text = str(time)
-        text = text if len(text) < 5 else text[:4]
-        ax_low.text(x[it] + 0.2, time + 1, text, fontsize=11, color='black', ha='center', rotation=90)
+            # Ivy runtimes
+            time = ivy_times[it]
+            text = str(time)
+            text = text if len(text) < 5 else text[:4]
+            ax_low.text(x[it] + 0.2, time + 1, text, fontsize=11, color='black', ha='center', rotation=90)
 
-    # Add diagonal break marks
-    d = 0.015  # Adjust diagonal size
-    kwargs = dict(transform=ax_high.transAxes, color='k', clip_on=False)
+        for idx, v in enumerate(ivy_times):
+            if v > 0: continue
+            text = '*' if v == 0 else '†'
+            ax_low.text(x[idx] + 0.2, 0.2, text, fontsize=14, color='black', ha='center')
 
-    # Top break marks (ax_high → lower boundary)
-    ax_high.plot((-d, +d), (-d, +d), **kwargs)  
-    ax_high.plot((1 - d, 1 + d), (-d, +d), **kwargs)  
+        # Add diagonal break marks
+        d = 0.015  # Adjust diagonal size
+        kwargs = dict(transform=ax_high.transAxes, color='k', clip_on=False)
 
-    kwargs = dict(transform=ax_low.transAxes, color='k', clip_on=False)
+        # Top break marks (ax_high → lower boundary)
+        ax_high.plot((-d, +d), (-d, +d), **kwargs)  
+        ax_high.plot((1 - d, 1 + d), (-d, +d), **kwargs)  
 
-    # Bottom break marks (ax_low → upper boundary)
-    ax_low.plot((-d, +d), (1 - d, 1 + d), **kwargs)  
-    ax_low.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  
+        kwargs = dict(transform=ax_low.transAxes, color='k', clip_on=False)
 
-    # Labels & Legend
-    ax_low.spines['top'].set_visible(False)
-    ax_high.spines['bottom'].set_visible(False)
-    ax_low.set_xticks(x)
-    ax_high.tick_params(axis='x', top=False, bottom=False, labelbottom=False)
-    plt.xticks(rotation=30, ha='right')
-    ax_low.set_xticklabels(list(categories.values()))
-    ax_low.set_ylabel("Time (s)")
-    ax_high.legend(prop={'size': 14}, loc=1, ncol=2)
+        # Bottom break marks (ax_low → upper boundary)
+        ax_low.plot((-d, +d), (1 - d, 1 + d), **kwargs)  
+        ax_low.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  
 
-    plt.savefig(output_file, dpi=300, bbox_inches='tight')
+        # Labels & Legend
+        ax_low.spines['top'].set_visible(False)
+        ax_high.spines['bottom'].set_visible(False)
+        ax_low.set_xticks(x)
+        ax_high.tick_params(axis='x', top=False, bottom=False, labelbottom=False)
+        plt.xticks(rotation=30, ha='right')
+        ax_low.set_xticklabels(list(categories.values()))
+        ax_low.set_ylabel("Time (s)")
+        ax_high.legend(prop={'size': 14}, loc=1, ncol=2)
+
+        plt.savefig(output_file, dpi=300, bbox_inches='tight')
+
+    def create_normalized_graph(output_file : str):
+        simp_times_normalized = np.array([(simp_times[it]) / (total_times[it]) for it in range(len(categories))])
+        trans_times_normalized = np.array([trans_times[it] / (total_times[it]) for it in range(len(categories))])
+        solving_time_normalized = np.array([solving_times[it] / (total_times[it]) for it in range(len(categories))])
+        ivy_times_normalized = np.array([ivy_times[it] / (total_times[it]) for it in range(len(categories))])
+
+        fig, (ax_high, ax_low) = plt.subplots(2, 1, sharex=True, figsize=(12, 6), gridspec_kw={'height_ratios': [1, 4], 'hspace': 0.05})
+        ax_low.set_ylim(0, 3)  # Normal range
+        ax_high.set_ylim(7, 10)  # Normal range
+
+        ax_high.bar(x - 0.2, simp_times_normalized, width=0.4, label='Simplification Time', color='blue')
+        ax_high.bar(x - 0.2, trans_times_normalized, width=0.4, bottom=simp_times_normalized, label='Translation Time', color='lightblue')
+        ax_high.bar(x - 0.2, solving_time_normalized, width=0.4, bottom=simp_times_normalized+trans_times_normalized, label='Solver Time', color='green')
+        ax_high.bar(x + 0.2, ivy_times_normalized, width=0.4, label='Ivy Runtime', color='red')
+
+        ax_low.bar(x - 0.2, simp_times_normalized, width=0.4, label='Simplification Time', color='blue')
+        ax_low.bar(x - 0.2, trans_times_normalized, width=0.4, bottom=simp_times_normalized, label='Translation Time', color='lightblue')
+        ax_low.bar(x - 0.2, solving_time_normalized, width=0.4, bottom=simp_times_normalized+trans_times_normalized,label='Solver Time', color='green')
+        ax_low.bar(x + 0.2, ivy_times_normalized, width=0.4, label='Ivy Runtime', color='red')
+
+        for idx, v in enumerate(ivy_times_normalized):
+            if v > 0: continue
+            text = '*' if v == 0 else '†'
+            ax_low.text(x[idx] + 0.2, 0.2, text, fontsize=14, color='black', ha='center')
+
+        for it in range(len(categories)):
+            text = str(total_times[it])
+            text = text if len(text) < 5 else text[:4]
+            ax_low.text(x[it] - 0.2, 1.2, text, fontsize=11, color='black', ha='center', rotation=90)
+            ivy_time_text = str(ivy_times[it])
+            ivy_time_text = ivy_time_text if len(ivy_time_text) < 5 else ivy_time_text[:4]
+            if ivy_times[it] > 0:
+                if ivy_times_normalized[it] + 0.2 <= 3:
+                    ax_low.text(x[it] + 0.28, ivy_times_normalized[it] + 0.2, ivy_time_text, fontsize=11, color='black', ha='center', rotation=90)
+                ax_high.text(x[it] + 0.28, ivy_times_normalized[it] + 0.2, ivy_time_text, fontsize=11, color='black', ha='center', rotation=90)
+
+        # Labels & Legend
+        # Add diagonal break marks
+        d = 0.015  # Adjust diagonal size
+        kwargs = dict(transform=ax_high.transAxes, color='k', clip_on=False)
+
+        # Top break marks (ax_high → lower boundary)
+        ax_high.plot((-d, +d), (-d, +d), **kwargs)  
+        ax_high.plot((1 - d, 1 + d), (-d, +d), **kwargs)  
+
+        kwargs = dict(transform=ax_low.transAxes, color='k', clip_on=False)
+
+        # Bottom break marks (ax_low → upper boundary)
+        ax_low.plot((-d, +d), (1 - d, 1 + d), **kwargs)  
+        ax_low.plot((1 - d, 1 + d), (1 - d, 1 + d), **kwargs)  
+
+        ax_low.spines['top'].set_visible(False)
+        ax_high.spines['bottom'].set_visible(False)
+
+        ax_low.set_xticks(x)
+        ax_high.tick_params(axis='x', top=False, bottom=False, labelbottom=False)
+        plt.xticks(rotation=30, ha='right')
+        ax_low.set_xticklabels(categories)
+        ax_low.set_ylabel("Normalised time")
+        # ax_high.set_title("Results for RQ1")
+        ax_high.legend(prop={'size': 14}, loc=1, ncol=2)
+
+        plt.savefig(output_file, dpi=300, bbox_inches='tight')
+        
+    
+    raw_filename = os.path.basename(output_file).replace(".", "_raw.")
+    create_raw_graph(raw_filename)
+
+    normalized_filename = os.path.basename(output_file).replace(".", "_normalized.")
+    create_normalized_graph(normalized_filename)
 
 # NOTE: Requires the benchmarks to have `set_option trace.profiler true`.
 if __name__ == "__main__":
@@ -175,7 +251,7 @@ if __name__ == "__main__":
                                         for j in {"simplification_time", "translation_time", "solving_time", "total_time", "total_ivy_time"}}
         print(dict(sorted(averaged_results.items())))
         if args.output_file:
-            create_graph(averaged_results, args.output_file)
+            create_graphs(averaged_results, args.output_file)
     else:
         if args.output_file:
             print("Cannot create graph for one benchmark.", file=sys.stderr)
