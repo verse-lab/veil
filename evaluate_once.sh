f4143384bbd37165df97654664823040f80a4fd6
George Pîrlea <george@pirlea.net>
2025-04-11 13:57:17 +0800

    eval: copy eval script results to `/tmp/output`

--- evaluate_once.sh
+++ evaluate_once.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 python3 ./scripts/eval.py Benchmarks/Veil/ --ivy-dir ./Benchmarks/Ivy/ --output-file evaluate_once.pdf 2> evaluate_once_log.txt | tee evaluate_once_results.txt
+mkdir -p /tmp/output
+cp evaluate_once_* /tmp/output/
--- smoke_test.sh
+++ smoke_test.sh
@@ -3,3 +3,5 @@
 mkdir -p /tmp/veil-smoke-test
 cp -r Benchmarks/Veil/Blockchain.lean Benchmarks/Veil/Ring.lean Benchmarks/Veil/RicartAgrawala.lean Benchmarks/Veil/MultiSigMajority.lean Benchmarks/Veil/TwoPhaseCommit.lean /tmp/veil-smoke-test/
 python3 ./scripts/eval.py /tmp/veil-smoke-test/ --ivy-dir ./Benchmarks/Ivy/ --output-file smoke_test.pdf 2> smoke_test_eval_log.txt | tee smoke_test_results.txt
+mkdir -p /tmp/output
+cp smoke_test_* /tmp/output/
