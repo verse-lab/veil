0507a6e8efe49bbff324eecdb6f10a4201575d1d
George Pîrlea <george@pirlea.net>
2025-06-27 20:10:05 +0800

    fix: add `nextLab` to `nextSimp`; fixes `already_proven_next_tr`

--- Veil/DSL/Base.lean
+++ Veil/DSL/Base.lean
@@ -5,7 +5,7 @@ attribute [initSimp, actSimp] RelationalTransitionSystem.init
 attribute [initSimp, invSimp, invSimpTopLevel] RelationalTransitionSystem.assumptions
 attribute [invSimp, invSimpTopLevel] RelationalTransitionSystem.inv
 attribute [invSimp, safeSimp, invSimpTopLevel] RelationalTransitionSystem.safe
-attribute [nextSimp, actSimp] RelationalTransitionSystem.next
+attribute [nextSimp, actSimp] RelationalTransitionSystem.next RelationalTransitionSystem.nextLab
 
 attribute [invSimp, safeSimp, initSimp, actSimp, invSimpTopLevel]
   RelationalTransitionSystem.invSafe RelationalTransitionSystem.invInit
