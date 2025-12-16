d4b05be409268b06323acbdb0d1f7569333493de
George Pîrlea <george@pirlea.net>
2025-12-16 12:15:39 +0800

    fix: convert `Veil.Enumeration` to `FinEnum`

--- Veil/Frontend/DSL/State/Types.lean
+++ Veil/Frontend/DSL/State/Types.lean
@@ -228,6 +228,8 @@ class Enumeration (α : Type u) where
 attribute [instance low] Enumeration.decEq
 attribute [grind ←] Enumeration.complete
 
+instance (priority := low) [enum : Enumeration α] : FinEnum α := FinEnum.ofList enum.allValues enum.complete
+
 /-!
 Here only gives some basic instances. More complicated ones should be
 found in `Veil.Frontend.Std`.
