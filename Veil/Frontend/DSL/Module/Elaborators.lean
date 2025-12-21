1a45868d27f100e96ef086450d61f77966226ae5
George Pîrlea <george@pirlea.net>
2025-12-21 23:08:38 +0800

    fix: race in `#check_invariants!`

--- Veil/Frontend/DSL/Module/Elaborators.lean
+++ Veil/Frontend/DSL/Module/Elaborators.lean
@@ -236,10 +236,12 @@ def elabCheckInvariants : CommandElab := fun stx => do
       liftCoreM <| addSuggestion stx cmds)
   | `(command|#check_invariants!) => do
     Verifier.displayStreamingResults stx getResults
-    Verifier.vcManager.atomically (fun ref => do
-      let mgr ← ref.get
-      let cmds ← liftCoreM <| constructCommands (← mgr.undischargedTheorems)
-      liftCoreM <| addSuggestion stx cmds)
+    Verifier.vcManager.atomicallyOnce frontendNotification
+      (fun ref => do let mgr ← ref.get; return mgr._doneWith.size == mgr.nodes.size)
+      (fun ref => do
+        let mgr ← ref.get
+        let cmds ← liftCoreM <| constructCommands (← mgr.undischargedTheorems)
+        liftCoreM <| addSuggestion stx cmds)
   | `(command|#check_invariants) => do
     Verifier.displayStreamingResults stx getResults
     -- Verifier.vcManager.atomicallyOnce frontendNotification
