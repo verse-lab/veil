6185f96222853804512da09457b2649a9733eb08
George Pîrlea <george@pirlea.net>
2025-12-30 23:13:17 +0800

    fix: switch to main fork of `lean-smt` and `lean-cvc5`

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL System
 
-require smt from git "https://github.com/zqy1018/lean-smt.git" @ "veil-2.0-v4.25.2"
+require smt from git "https://github.com/dranov/lean-smt.git" @ "veil-2.0-v4.25.2"
 require Loom from git "https://github.com/verse-lab/loom-dev.git" @ "extract-list-v4.25.2"
 
 package veil
